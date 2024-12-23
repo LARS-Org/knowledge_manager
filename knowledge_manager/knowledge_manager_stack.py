54cf28ce83da5e532dd4379a2bacede72b7fc5a3
Amg <andersonmg@gmail.com>
2024-12-23 18:36:10 -0300

    WIP: initial version

--- knowledge_manager/knowledge_manager_stack.py
+++ knowledge_manager/knowledge_manager_stack.py
@@ -6,16 +6,18 @@ from constructs import Construct
 
 from app_common.app_common_stack import AppCommonStack
 
+
 class KnowledgeManagerStack(AppCommonStack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create the SNS topic "AddAIJobLastMessagesAttachedTopic"
-        ai_job_last_msgs_topic = self._get_or_create_sns_topic("AddAIJobLastMessagesAttachedTopic")
-
-        
+        ai_job_last_msgs_topic = self._get_or_create_sns_topic(
+            "AddAIJobLastMessagesAttachedTopic"
+        )
 
         # Create the SNS topic "AIJobContextAttachedTopic"
-        ai_job_context_attached_topic = self._get_or_create_sns_topic_with_sms_param("AIJobContextAttachedTopic")
-
+        ai_job_context_attached_topic = self._get_or_create_sns_topic_with_sms_param(
+            "AIJobContextAttachedTopic"
+        )
