37642baa17085aa3f637d810f05e85c509fe7ca6
Amg <andersonmg@gmail.com>
2025-01-17 06:12:08 -0300

    Add AppRoleRetrieverLambda

--- knowledge_manager/knowledge_manager_stack.py
+++ knowledge_manager/knowledge_manager_stack.py
@@ -17,14 +17,13 @@ class KnowledgeManagerStack(AppCommonStack):
         # the sort key will be the source_type of the role ("txt_gdrive", "table" etc)
         # The table must have the attribute "source" to persist the source of the role itself
         app_role_table = self._create_dynamodb_table(
-            table_name="AppRoleTable",
+            table_name="AppRoleTable-v1",
             pk_name="app_id",
             pk_type=dynamodb.AttributeType.STRING,
-            sk_name="source_type",
-            sk_type=dynamodb.AttributeType.STRING,
         )
 
-        # Create a lambda, using the method self._create_lambda, responsible for retrieve the AppRoles registers based on the app_id
+        # Create a lambda, using the method self._create_lambda, responsible 
+        # for retrieve the AppRole registered for the app_id
         # The lambda will be named "AppRoleRetriever"
         self._create_lambda(
             name="AppRoleRetrieverLambda",
---
+++ lambdas/app_role_retriever.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+# Adds lambda/create/packages to the system path.
+# This must be done before importing any modules from the packages directory.
+sys.path.append(os.path.join(os.path.dirname(__file__), "packages"))
+
+from lambdas.app_role_bo import AppRoleBO
+from app_common.base_lambda_handler import BaseLambdaHandler
+
+
+class AppRoleRetriever(BaseLambdaHandler):
+    """
+    This class is responsible for
+    """
+
+    def _handle(self) -> dict:
+        """ """
+        app_id = self.body.get("app_id", None)
+
+        if not app_id:
+            raise ValueError("app_id is required")
+
+        # else: app_id is present
+
+        app_role_table_name = self.get_env_var("APP_ROLE_TABLE_NAME")
+        app_role_bo = AppRoleBO(table_name=app_role_table_name)
+
+        app_role = app_role_bo.get_role_content(app_id=app_id)
+
+        if not app_role:
+            raise ValueError(f"AppRole not found for app_id: {app_id}")
+
+        payload = self.body | {"app_role": app_role}
+
+        # Publish the response to the event bus
+        self.publish_to_custom_event_bus(
+            message=payload,
+            detail_type="AppRoleRetrieved",
+        )
+
+        return payload
+
+
+def handler(event, context):
+    """
+    Lambda function to route messages from the multi-channel SQS queue to the appropriate handler.
+    """
+    _handler = AppRoleRetriever()
+    # Implicitly invokes __call__() ...
+    #   ... which invokes _do_the_job() ...
+    #     ... which invokes before_handle(), handle() and after_handle()
+    return _handler(event, context)
