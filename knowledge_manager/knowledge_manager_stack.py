b18faa18d21f15b88f297649af32222e6e5ea884
Amg <andersonmg@gmail.com>
2025-01-17 05:30:45 -0300

    WIP: add AppRoleTable

--- knowledge_manager/knowledge_manager_stack.py
+++ knowledge_manager/knowledge_manager_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import (
-    Stack,
-    aws_sns_subscriptions as sns_subscriptions,
+    aws_dynamodb as dynamodb,
 )
 from constructs import Construct
 
@@ -12,12 +11,31 @@ class KnowledgeManagerStack(AppCommonStack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # Create the SNS topic "AddAIJobLastMessagesAttachedTopic"
-        ai_job_last_msgs_topic = self._get_or_create_sns_topic(
-            "AddAIJobLastMessagesAttachedTopic"
+        # create a table using the self._create_dynamodb_table method
+        # The table will be named "AppRoleTable"
+        # the partition key will be the app_id
+        # the sort key will be the source_type of the role ("txt_gdrive", "table" etc)
+        # The table must have the attribute "source" to persist the source of the role itself
+        app_role_table = self._create_dynamodb_table(
+            table_name="AppRoleTable",
+            pk_name="app_id",
+            pk_type=dynamodb.AttributeType.STRING,
+            sk_name="source_type",
+            sk_type=dynamodb.AttributeType.STRING,
         )
 
-        # Create the SNS topic "AIJobContextAttachedTopic"
-        ai_job_context_attached_topic = self._get_or_create_sns_topic_with_sms_param(
-            "AIJobContextAttachedTopic"
+        # Create a lambda, using the method self._create_lambda, responsible for retrieve the AppRoles registers based on the app_id
+        # The lambda will be named "AppRoleRetriever"
+        self._create_lambda(
+            name="AppRoleRetrieverLambda",
+            handler="app_role_retriever.handler",
+            environment={
+                "APP_ROLE_TABLE_NAME": app_role_table.table_name,
+            },
         )
+
+
+
+        
+
+
